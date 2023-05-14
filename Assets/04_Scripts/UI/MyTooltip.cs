a59c2224a5e782c96bb4c608c847cb2dcc3f2e87
fcumselab <fcuselab245@gmail.com>
2023-05-15 04:08:27 +0800

    Add new function to control selection List position(list won't go to outside.)

---
+++ Assets/04_Scripts/UI/MyTooltip.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTooltip : MonoBehaviour
+{
+    [SerializeField] RectTransform GameScreenRectTransform;
+    [SerializeField] RectTransform TooltipRectTransform;
+    RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 anchoredPos = Input.mousePosition / GameScreenRectTransform.localScale.x;
+
+        if(anchoredPos.x + rectTransform.rect.width > GameScreenRectTransform.rect.width)
+        {
+            anchoredPos.x = GameScreenRectTransform.rect.width - rectTransform.rect.width;
+        }
+
+        if (anchoredPos.y + rectTransform.rect.height > GameScreenRectTransform.rect.height)
+        {
+            anchoredPos.y = GameScreenRectTransform.rect.height - rectTransform.rect.height;
+        }
+
+        TooltipRectTransform.anchoredPosition = anchoredPos;
+    }
+
+    
+}
