5e89be9e938e83c98458bb11bbe7ce4282eb5d0f
fcumselab <fcuselab245@gmail.com>
2023-06-14 14:17:02 +0800

    All stage UI & interaction complete

--- Assets/04_Scripts/Tool/Tool.cs
+++ Assets/04_Scripts/Tool/Tool.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Tool : MonoBehaviour
 {
+    [SerializeField] List<string> completeQuestNameList;
+    [SerializeField] List<string> completeQuestTimeList;
+    [SerializeField] List<string> completeQuestTypeList;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +24,27 @@ public class Tool : MonoBehaviour
     public string[] SplitStringIntoArray(string word, string keyword)
     {
         return word.Split(new string[] { keyword }, StringSplitOptions.None);
+    }
+
+    public void AddCompleteQuestData(string questName, string time, string type)
+    {
+        completeQuestNameList.Add(questName);
+        completeQuestTimeList.Add(time);
+        completeQuestTypeList.Add(type);
+    }
+
+    public string[] GetCompleteQuestName()
+    {
+        return completeQuestNameList.ToArray();
+    }
+
+    public string[] GetCompleteQuestTime()
+    {
+        return completeQuestTimeList.ToArray();
+    }
+
+    public string[] GetCompleteQuestType()
+    {
+        return completeQuestTypeList.ToArray();
     }
 }
