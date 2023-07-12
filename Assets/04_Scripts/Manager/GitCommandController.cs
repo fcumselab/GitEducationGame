5e1c370a01674a2a84bde9aba25079b369259b91
fcumselab <fcuselab245@gmail.com>
2023-07-12 17:43:40 +0800

    Random Quests & Enter command bug fix & Quest Name follow Random obj.
    1. Random 5 Quests completed.
    2. Enter command function won't run when there's not current quest.
    3. Now Quest name will follow the data from Random object file.

--- Assets/04_Scripts/Manager/GitCommandController.cs
+++ Assets/04_Scripts/Manager/GitCommandController.cs
@@ -79,8 +79,13 @@ public class GitCommandController : SerializedMonoBehaviour
         {
             if (commandList[0] == "git")
             {
-                if(commandList[1] == "--version" || commandList[1] == "-v") resultList.Add("findOne");
-                else resultList.Add("findZero");
+                if (commandList.Count == 1) resultList.Add("findZero");
+                else
+                {
+                    if (commandList[1] == "--version" || commandList[1] == "-v") resultList.Add("findOne");
+                    else resultList.Add("findZero");
+                }
+                
             } 
             else
             {
