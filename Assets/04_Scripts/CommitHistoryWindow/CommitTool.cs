9c0be084fa08503a7cc9009d7ce67a8ecc9730ac
fcumselab <fcuselab245@gmail.com>
2023-08-02 21:05:24 +0800

    CommitHistoryPanel will auto resize (using git log command)

--- Assets/04_Scripts/CommitHistoryWindow/CommitTool.cs
+++ Assets/04_Scripts/CommitHistoryWindow/CommitTool.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HutongGames.PlayMaker;
 
 public class CommitTool : MonoBehaviour
 {
@@ -25,5 +26,74 @@ public class CommitTool : MonoBehaviour
                 return result;
             }
         }
+    }
+
+    public Vector2 SetCommitHistoryPanelSize(GameObject commitHistory)
+    {
+        Dictionary<string, int> branchCountDic = new();
+
+        Transform Commits = commitHistory.transform.Find("Commits");
+        for(int x=0; x< Commits.childCount; x++)
+        {
+            Transform child = Commits.GetChild(x);
+            if (child.gameObject.activeSelf)
+            {
+                PlayMakerFSM[] fsm = child.GetComponents<PlayMakerFSM>();
+                PlayMakerFSM targetFsm = null;
+                for (int y=0; y < fsm.Length; y++)
+                {
+                    if(fsm[y].FsmName == "Content")
+                    {
+                        targetFsm = fsm[y];
+                        break;
+                    }
+                }
+
+                for(int y=0; y< targetFsm.FsmVariables.GetFsmArray("branchList").Length; y++)
+                {
+                    string branchName = (string)targetFsm.FsmVariables.GetFsmArray("branchList").Get(y);
+                    if (branchCountDic.ContainsKey(branchName))
+                    {
+                        branchCountDic[branchName]++;
+                    }
+                    else
+                    {
+                        branchCountDic.Add(branchName, 1);
+                    }
+                }
+            }
+        }
+        int maxHeightLen = 0;
+        int maxWidthLen = 0;
+        foreach (var branch in branchCountDic)
+        {
+            if(maxHeightLen < branch.Value) maxHeightLen = branch.Value;
+        }
+
+        Transform Branches = commitHistory.transform.Find("Branches");
+        for (int x = 0; x < Branches.childCount; x++)
+        {
+            Transform child = Branches.GetChild(x);
+            if (child.gameObject.activeSelf)
+            {
+                PlayMakerFSM[] fsm = child.GetComponents<PlayMakerFSM>();
+                PlayMakerFSM targetFsm = null;
+                for (int y = 0; y < fsm.Length; y++)
+                {
+                    if (fsm[y].FsmName == "Branch")
+                    {
+                        targetFsm = fsm[y];
+                        break;
+                    }
+                }
+                int branchId = targetFsm.FsmVariables.GetFsmInt("branchId").ToInt();
+                if (maxWidthLen < branchId) maxWidthLen = branchId;
+            }
+        }
+        maxWidthLen = ((maxWidthLen + 1) * 175);
+        maxHeightLen *= 175;
+        Vector2 commitHistorySize = new(maxWidthLen, maxHeightLen);
+        return commitHistorySize;
+
     }
 }
