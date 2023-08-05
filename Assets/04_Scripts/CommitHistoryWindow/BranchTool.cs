ef6cc6fac305f67fef621c1956ebe16e502d3f92
fcumselab <fcuselab245@gmail.com>
2023-08-05 21:30:21 +0800

    Update Commits color when checkout branch successful.

---
+++ Assets/04_Scripts/CommitHistoryWindow/BranchTool.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using HutongGames.PlayMaker;
+using UnityEngine.UI;
+
+public class BranchTool : SerializedMonoBehaviour
+{
+    [SerializeField] Dictionary<string, GameObject> CommitDict = new();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool AddCommit(string commitId, GameObject Commit)
+    {
+        CommitDict.Add(commitId, Commit);
+        return true;
+    }
+
+    public bool UpdateCommitsColor(string branchName, string targetCommit, Color selectedColor, Color grayColor)
+    {
+        Image image;
+        Text text;
+        foreach (var commit in CommitDict)
+        {
+            image = commit.Value.transform.Find("Image").GetComponent<Image>();
+            image.color = grayColor;
+
+            text = commit.Value.transform.Find("Text").GetComponent<Text>();
+            text.color = selectedColor;
+            text.text = branchName[0].ToString();
+        }
+
+        GameObject TargetCommit = CommitDict[targetCommit];
+        image = TargetCommit.transform.Find("Image").GetComponent<Image>();
+        image.color = selectedColor;
+
+        text = TargetCommit.transform.Find("Text").GetComponent<Text>();
+        text.color = grayColor;
+        text.text = branchName[0].ToString();
+
+        return true;
+    }
+}
